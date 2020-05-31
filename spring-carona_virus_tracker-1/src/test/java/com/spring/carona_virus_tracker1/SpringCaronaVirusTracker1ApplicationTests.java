0821d192a1220c192f4a247f7f2ecc3881aeb2e7
dhiraj kumar <47515998+neerjeet@users.noreply.github.com>
2020-05-31 11:10:39 +0530

    Add files via upload

---
+++ spring-carona_virus_tracker-1/src/test/java/com/spring/carona_virus_tracker1/SpringCaronaVirusTracker1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.carona_virus_tracker1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCaronaVirusTracker1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
